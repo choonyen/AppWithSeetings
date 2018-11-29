a289c9cc533a8f9d7638e9c9c2985e58da2ce692
anreh <anurehka@gmail.com>
2018-11-29 13:46:39 +0800

    Initial commit

---
+++ app/src/main/java/com/example/taruc/appwithseetings/SettingsFragment.java
@@ -0,0 +1,31 @@
+package com.example.taruc.appwithseetings;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.preference.PreferenceFragmentCompat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SettingsFragment extends PreferenceFragmentCompat {
+
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+
+    }
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportFragmentManager().beginTransaction()
+                .replace(android.R.id.content, new SettingsFragment())
+                .commit();
+    }
+
+
+}
